8c5aa915a162383d8933de1eb7f62236a1263749
samdunk47 <98485063+samdunk47@users.noreply.github.com>
2022-11-28 20:46:21 +0000

    e

--- hello.cpp
+++ hello.cpp
@@ -1,19 +1,17 @@
 #include <iostream>
 #include <string>
 #include <cmath>
-using namespace std;
 
 class MyClass
 {
 public:
     int myNum;
-    string myString;
     void myMethod();
 };
 
 void MyClass::myMethod()
 {
-    cout << myNum << " " << myString << endl;
+    std::cout << myNum << std::endl;
 }
 
 int main()
@@ -21,7 +19,6 @@ int main()
     MyClass myObj;
 
     myObj.myNum = 15;
-    myObj.myString = "Hello, world!";
 
     myObj.myMethod();
 
