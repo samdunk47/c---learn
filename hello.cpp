b2fba98e04b16fad9242332341d4ce42f4a45725
samdunk47 <98485063+samdunk47@users.noreply.github.com>
2022-11-29 21:00:06 +0000

    e

--- hello.cpp
+++ hello.cpp
@@ -1,30 +1,28 @@
 #include <iostream>
 #include <string>
 #include <cmath>
-using namespace std;
 
-class MyClass
+class Car
 {
 public:
-    int myNum;
-    string myString;
-    void myMethod();
+    std::string brand;
+    std::string model;
+    int year;
+    Car(std::string x, std::string y, int z)
+    {
+        brand = x;
+        model = y;
+        year = z;
+    }
 };
-
-void MyClass::myMethod()
-{
-    cout << myNum << " " << myString << endl;
-}
 
 int main()
 {
-    MyClass myObj;
+    Car carObj1("BMW", "X5", 1999);
+    Car carObj2("Ford", "Mustang", 1969);
 
-    myObj.myNum = 15;
-    myObj.myString = "Hello, world!";
-
-    myObj.myMethod();
+    std::cout << carObj1.brand << " " << carObj1.model << " " << carObj1.year << " " << std::endl;
+    std::cout << carObj2.brand << " " << carObj2.model << " " << carObj2.year << " " << std::endl;
 
     return 0;
 }
-
