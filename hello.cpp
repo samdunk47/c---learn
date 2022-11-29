9b514294444ccb0108a500d3d5f0a95fce472969
samdunk47 <98485063+samdunk47@users.noreply.github.com>
2022-11-29 21:07:08 +0000

    e

--- hello.cpp
+++ hello.cpp
@@ -2,14 +2,17 @@
 #include <string>
 #include <cmath>
 
-class Car {
-  public:
-    string brand;
-    string model;
-    int year;
-    Car(string x, string y, int z);
+using namespace std;
+
+class Car {        // The class
+  public:          // Access specifier
+    std::string brand;  // Attribute
+    std::string model;  // Attribute
+    int year;      // Attribute
+    Car(string x, string y, int z); // Constructor declaration
 };
 
+// Constructor definition outside the class
 Car::Car(string x, string y, int z) {
   brand = x;
   model = y;
@@ -17,11 +20,12 @@ Car::Car(string x, string y, int z) {
 }
 
 int main() {
+  // Create Car objects and call the constructor with different values
   Car carObj1("BMW", "X5", 1999);
   Car carObj2("Ford", "Mustang", 1969);
 
-  cout << carObj1.brand << " " << carObj1.model << " " << carObj1.year << "\n";
-  stdcout << carObj2.brand << " " << carObj2.model << " " << carObj2.year << "\n";
-  
+  // Print values
+  std::cout << carObj1.brand << " " << carObj1.model << " " << carObj1.year << std::endl;
+  std::cout << carObj2.brand << " " << carObj2.model << " " << carObj2.year << std::endl;
   return 0;
 }
