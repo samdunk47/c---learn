a2be5515b5932e4ccf7aa4681ecddb43dafc3330
samdunk47 <98485063+samdunk47@users.noreply.github.com>
2022-11-29 21:15:44 +0000

    asef

--- hello.cpp
+++ hello.cpp
@@ -8,23 +8,19 @@ public:
     std::string brand;
     std::string model;
     int year;
-    Car(std::string x, std::string y, int z);
+    Car(std::string x, std::string y, int z)
+    {
+        brand = x;
+        model = y;
+        year = z;
+    }
 };
-
-Car::Car(std::string x, std::string y, int z)
-{
-    brand = x;
-    model = y;
-    year = z;
-}
 
 int main()
 {
-    // Create Car objects and call the constructor with different values
     Car carObj1("BMW", "X5", 1999);
     Car carObj2("Ford", "Mustang", 1969);
 
-    // Print values
     std::cout << carObj1.brand << " " << carObj1.model << " " << carObj1.year << std::endl;
     std::cout << carObj2.brand << " " << carObj2.model << " " << carObj2.year << std::endl;
     return 0;
